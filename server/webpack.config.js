561e6ad6a28db11d9bf7bf2fabfd32d4dc651a12
Anon Karytski <heyheyjude23@gmail.com>
2021-04-21 00:26:38 +0300

    ready to deploy

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   target: "node",
@@ -6,6 +7,9 @@ module.exports = {
   entry: "./app.js",
   resolve: {
     modules: ["server", "node_modules"],
+    alias: {
+      "pg-native": path.join(__dirname, "aliases/pg-native.js"),
+    },
   },
   plugins: [new webpack.IgnorePlugin(/^pg-native$/)],
 };
