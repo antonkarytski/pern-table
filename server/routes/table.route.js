41e8d580eb9273e85eacbb59a288ec1dabab53a4
Anon Karytski <heyheyjude23@gmail.com>
2021-04-21 01:24:25 +0300

    test log

--- server/routes/table.route.js
+++ server/routes/table.route.js
@@ -3,11 +3,16 @@ const router = Router();
 const pool = require("../db");
 
 router.get("/allrecords", async (req, res) => {
-  const allRecords = await pool.query(
-    'SELECT * FROM "public"."pern_table" LIMIT 100'
-  );
-  console.log(allRecords.rows);
-  res.json(allRecords.rows);
+  console.log("start");
+  try{
+    const allRecords = await pool.query(
+      'SELECT * FROM "public"."pern_table" LIMIT 100'
+    );
+    console.log(allRecords.rows);
+    res.json(allRecords.rows);
+  } catch (e) {
+    console.log(e)
+  }
 });
 
 module.exports = router;
